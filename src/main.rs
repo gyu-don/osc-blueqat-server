3a3e14e8a820458bdf18d506f544c7d762c41eb9
gyu-don <takumi.kt+git@gmail.com>
2020-12-31 21:35:45 +0900

    updated

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::str::FromStr;
 use tokio::task;
 use tokio::net::UdpSocket;
 use tokio::sync::mpsc;
+use tokio::signal::ctrl_c;
 
 use anyhow::{anyhow, bail, ensure};
 
@@ -25,29 +26,49 @@ type Qubit = <BlueqatOperations as Operations>::Qubit;
 const QUEUE_LEN: usize = 100;
 const OSC_BUF_LEN: usize = 1000;
 
+/// Loop for sending response to client.
 async fn sender_loop(tx_addr: SocketAddr, mut chan_rx: mpsc::Receiver<Response>) -> anyhow::Result<()> {
-    let tx = UdpSocket::bind(tx_addr).await?;
+    //let tx = UdpSocket::bind(tx_addr).await?;
+    let tx = std::net::UdpSocket::bind("0.0.0.0:9999")?;
     while let Some(msg) = chan_rx.recv().await {
-        info!("host_sender_loop: Received from channel: {:?}", msg);
+        info!("sender_loop: Received from channel: {:?}", msg);
+        /*
         let packet = rosc::encoder::encode(&OscPacket::Bundle(
                 OscBundle { timetag: (0, 0),
                             content: vec![OscPacket::Message(OscMessage::from(&msg))]
                 })).map_err(|e| anyhow!("{:?}", e))?;
-        tx.send(&packet).await?;
+        */
+        let packet = rosc::encoder::encode(&OscPacket::Message(OscMessage::from(&msg)))
+            .map_err(|e| anyhow!("{:?}", e))?;
+        info!("sender_loop: Encoded packet (len={}): {:?}", packet.len(), packet);
+        info!("sender_loop: Sending to {}...", tx_addr);
+        //tx.send(&packet).await?;
+        tx.send_to(&packet, tx_addr)?;
+        info!("sender_loop: Sent.");
     }
-    bail!("host_sender_loop unexpected finished");
+    bail!("sender_loop: unexpected finished");
 }
 
+/// Loop for receiving request from client.
 async fn receiver_loop(host_rx_addr: SocketAddr, chan_tx: mpsc::Sender<Request>) -> anyhow::Result<()> {
-    let rx = UdpSocket::bind(host_rx_addr).await?;
     let mut buf = vec![0; OSC_BUF_LEN];
+    let rx = UdpSocket::bind(host_rx_addr).await?;
     loop {
-        info!("Receiving from {}...", host_rx_addr);
+        info!("receiver_loop: Receiving from {}...", host_rx_addr);
         let len = rx.recv(&mut buf).await?;
-        let packet = rosc::decoder::decode(&buf[..len]).map_err(|e| anyhow!("{:?}", e))?;
+        info!("receiver_loop: Received. len={}, bytes={:?}", len, &buf[..len]);
+        let packet = rosc::decoder::decode(&buf[..len]);
+        let packet = match packet {
+            Ok(inner) => inner,
+            Err(e) => {
+                warn!("receiver_loop: OSC Error {:?}", e);
+                continue;
+            }
+        };
+        info!("receiver_loop: OSC Message: {:?}", packet);
         let msg = Request::try_from(match packet {
             OscPacket::Message(msg) => {
-                warn!("Message without Bundle");
+                warn!("receiver_loop: Message without Bundle");
                 msg
             },
             OscPacket::Bundle(mut bundle) => {
@@ -59,17 +80,19 @@ async fn receiver_loop(host_rx_addr: SocketAddr, chan_tx: mpsc::Sender<Request>)
                 }
             }
         })?;
-        buf.clear();
+        info!("receiver_loop: Message: {:?}", msg);
         chan_tx.send(msg).await?;
     }
 }
 
-async fn runner_loop(mut ops_rx: mpsc::Receiver<Request>, mut result_tx: mpsc::Sender<Response>) -> anyhow::Result<()> {
+async fn runner_loop(mut ops_rx: mpsc::Receiver<Request>, result_tx: mpsc::Sender<Response>) -> anyhow::Result<()> {
     let mut sim = BlueqatSimulator::new().unwrap();
     let mut ops = BlueqatOperations::new();
     let mut result = String::new();
     ops.initialize();
+    info!("runner_loop: Start");
     while let Some(msg) = ops_rx.recv().await {
+        info!("runner_loop: Message received from channel. {:?}", msg);
         match msg {
             Request::X(_, n) => ops.x(n as Qubit),
             Request::Y(_, n) => ops.y(n as Qubit),
@@ -77,10 +100,13 @@ async fn runner_loop(mut ops_rx: mpsc::Receiver<Request>, mut result_tx: mpsc::S
             Request::H(_, n) => ops.h(n as Qubit),
             Request::CX(_, n1, _, n2) => ops.cx(n1 as Qubit, n2 as Qubit),
             Request::Mz(_, n) => {
+                info!("runner_loop: Received Mz inst.");
                 ops.measure(n as Qubit, ());
+                info!("runner_loop: Calling blueqat...");
                 sim.send_receive(&ops, &mut result).await;
-                //let bit = (result.as_bytes()[n as usize] - b'0') as u32;
-                result_tx.send(Response::Mz(0, 0.0)).await?;
+                info!("runner_loop: Blueqat response: {}", result);
+                let bit = (result.as_bytes()[n as usize] - b'0') as i32;
+                result_tx.send(Response::Mz(bit, 0.0)).await?;
                 ops = BlueqatOperations::new();
                 result.clear();
             },
@@ -101,12 +127,13 @@ async fn main() -> anyhow::Result<()> {
                         .expect("Receiver addr required.");
     let (ops_tx, ops_rx) = mpsc::channel(QUEUE_LEN);
     let (result_tx, result_rx) = mpsc::channel(QUEUE_LEN);
-    let receiver = task::spawn(receiver_loop(rx, ops_tx));
-    let runner = task::spawn(runner_loop(ops_rx, result_tx));
     let sender = task::spawn(sender_loop(tx, result_rx));
+    let runner = task::spawn(runner_loop(ops_rx, result_tx));
+    let receiver = task::spawn(receiver_loop(rx, ops_tx));
 
-    receiver.await??;
-    runner.await??;
-    sender.await??;
+    ctrl_c().await?;
+    receiver.abort();
+    runner.abort();
+    sender.abort();
     Ok(())
 }
